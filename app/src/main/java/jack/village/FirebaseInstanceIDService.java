b7a8b1f4c7f81acfa96849862d6bddebecc155c6
jackrobb <jrobb6696@gmail.com>
2018-02-09 21:34:07 +0000

    Removed unnecessary code

--- app/src/main/java/jack/village/FirebaseInstanceIDService.java
+++ app/src/main/java/jack/village/FirebaseInstanceIDService.java
@@ -1,8 +1,5 @@
 package jack.village;
 
-import android.util.Log;
-
-import com.google.firebase.iid.FirebaseInstanceId;
 
 /**
  * Created by jack on 02/02/2018.
@@ -10,16 +7,11 @@ import com.google.firebase.iid.FirebaseInstanceId;
 
 public class FirebaseInstanceIDService extends com.google.firebase.iid.FirebaseInstanceIdService {
 
-        private static final String TAG = "MyFirebaseIIDService";
+
 
         @Override
         public void onTokenRefresh() {
 
-            //Getting registration token (Unique ID allowing it to receive messages)
-            String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-
-            //Displaying token in logcat
-            Log.e(TAG, "Refreshed token: " + refreshedToken);
 
         }
 
