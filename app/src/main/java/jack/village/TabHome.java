61cda46078b094145152719231b1cdf19a718ec2
jackrobb <jrobb6696@gmail.com>
2018-02-16 18:36:16 +0000

    Added support for remote configuration

--- app/src/main/java/jack/village/TabHome.java
+++ app/src/main/java/jack/village/TabHome.java
@@ -2,10 +2,16 @@ package jack.village;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 
 /**
@@ -13,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class TabHome extends Fragment {
 
+    TextView title, description, paragraphOne, paragraphTwo;
+    FirebaseRemoteConfig firebaseRemoteConfig;
 
     public TabHome() {
         // Required empty public constructor
@@ -25,7 +33,39 @@ public class TabHome extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tab_home, container, false);
 
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+        title = view.findViewById(R.id.home_title);
+        description = view.findViewById(R.id.home_description);
+        paragraphOne = view.findViewById(R.id.home_paragraphOne);
+        paragraphTwo = view.findViewById(R.id.home_paragraphTwo);
+
+        //Fetch the data from remote configuration
+        firebaseRemoteConfig.fetch()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            firebaseRemoteConfig.activateFetched();
+                            Log.d("Config", "Successful");
+                        }else{
+                            Log.d("Config", "Unsuccessful");
+                        }
+                        fetchContent();
+                    }
+                });
+
         return view;
+    }
+
+    public void fetchContent(){
+        //Update Text Fields
+        title.setText(firebaseRemoteConfig.getString("Title"));
+        description.setText(firebaseRemoteConfig.getString("Description"));
+        paragraphOne.setText(firebaseRemoteConfig.getString("ParagraphOne"));
+        paragraphTwo.setText(firebaseRemoteConfig.getString("ParagraphTwo"));
     }
 
 }
