da73495bd7b00c46b563887efd8f1ac9b8ea3525
jackrobb <jrobb6696@gmail.com>
2018-02-21 18:44:37 +0000

    Updated podcasts to include seek bar and current/total duration times

--- app/src/main/java/jack/village/PodcastPlayer.java
+++ app/src/main/java/jack/village/PodcastPlayer.java
@@ -4,17 +4,31 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class PodcastPlayer extends AppCompatActivity {
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class PodcastPlayer extends AppCompatActivity{
 
     private TextView title;
     private TextView description;
+    private  TextView currentDuration;
+    private TextView totalDuration;
     private ImageButton play;
+    private ProgressBar podcastProgress;
+
+    private SeekBar podcastSeek;
+
+    private Handler handler;
 
     private MediaPlayer mediaPlayer;
 
@@ -27,6 +41,36 @@ public class PodcastPlayer extends AppCompatActivity {
 
         title = findViewById(R.id.podcastTitle);
         description = findViewById(R.id.podcastDescription);
+
+        currentDuration = findViewById(R.id.currentDuration);
+        totalDuration = findViewById(R.id.totalDuration);
+
+        handler = new Handler();
+
+        podcastSeek = findViewById(R.id.podcastSeek);
+
+        podcastSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean input) {
+                if(input){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                handler.removeCallbacks(mUpdateTimeTask);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                updateProgressBar();
+            }
+        });
+
+        podcastProgress = findViewById(R.id.podcastProgress);
+
+        podcastProgress.setVisibility(View.VISIBLE);
 
         //Create new media player
         mediaPlayer = new MediaPlayer();
@@ -67,22 +111,49 @@ public class PodcastPlayer extends AppCompatActivity {
 
     }
 
+    public void updateProgressBar() {
+        handler.postDelayed(mUpdateTimeTask, 0);
+    }
+
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+
+            if(mediaPlayer != null) {
+
+                String total = String.format(Locale.getDefault(), "%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition()) % 60,
+                        TimeUnit.MILLISECONDS.toSeconds(mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition()) % 60);
+
+                String current = String.format(Locale.getDefault(), "%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(mediaPlayer.getCurrentPosition()) % 60,
+                        TimeUnit.MILLISECONDS.toSeconds(mediaPlayer.getCurrentPosition()) % 60);
+
+                totalDuration.setText(total);
+                currentDuration.setText(current);
+
+                //Set the position of the seek bar to the current position of the media player
+                podcastSeek.setProgress(mediaPlayer.getCurrentPosition());
+
+                handler.postDelayed(this, 0);
+            }
+        }
+    };
+
     @Override
     protected void onPause() {
         super.onPause();
-
-        //If the user leaves the activity and the media player is not null, reset and release the media player and set it to null
-        if (mediaPlayer != null) {
-            mediaPlayer.reset();
-            mediaPlayer.release();
-            mediaPlayer = null;
-        }
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         this.finish();
+        //Stop the media player when the user goes off the activity
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
     class Player extends AsyncTask<String, Void, Boolean> {
@@ -117,7 +188,20 @@ public class PodcastPlayer extends AppCompatActivity {
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
+            podcastProgress.setVisibility(View.GONE);
+            podcastSeek.setProgress(0);
+            podcastSeek.setMax(mediaPlayer.getDuration());
+            updateProgressBar();
             mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.seekTo(0);
+                    mediaPlayer.pause();
+                    play.setImageResource(R.drawable.ic_action_play);
+                }
+            });
         }
 
         @Override
