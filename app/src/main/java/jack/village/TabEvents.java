fd38e03c2c57f9602f9725935d65c50b6de1ce98
jackrobb <jrobb6696@gmail.com>
2018-02-23 21:38:08 +0000

    Events load within Webview

--- app/src/main/java/jack/village/TabEvents.java
+++ app/src/main/java/jack/village/TabEvents.java
@@ -6,24 +6,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TabEvents extends Fragment {
 
+    WebView webView;
 
     public TabEvents() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab_events, container, false);
+
+        webView = view.findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl("https://www.villagebelfast.com/events/");
+
+        return view;
+
     }
 
 }
