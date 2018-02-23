b99f3331793280a44465ef967d2356930185093e
jackrobb <jrobb6696@gmail.com>
2018-02-23 13:28:40 +0000

    Removed unused imports

--- app/src/main/java/jack/village/PodcastPlayer.java
+++ app/src/main/java/jack/village/PodcastPlayer.java
@@ -7,8 +7,6 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
