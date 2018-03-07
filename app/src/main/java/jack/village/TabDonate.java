7852f03b03db1d65fa9566a8b1d69f9a2914766e
jackrobb <jrobb6696@gmail.com>
2018-03-07 15:16:50 +0000

    New font. Donations page started.

--- app/src/main/java/jack/village/TabDonate.java
+++ app/src/main/java/jack/village/TabDonate.java
@@ -1,16 +1,46 @@
 package jack.village;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.CardRequirements;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.TransactionInfo;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+import com.stripe.android.model.Token;
+
+import java.util.Arrays;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TabDonate extends Fragment {
+
+    private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 53;
+
+    private View payButton;
+    private PaymentsClient PaymentsClient;
+    private NumberPicker numberPicker;
+    private int amount;
 
     public TabDonate() {
         // Required empty public constructor
@@ -23,8 +53,141 @@ public class TabDonate extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tab_donate, container, false);
 
+        numberPicker = view.findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(30);
+
+        PaymentsClient =
+                Wallet.getPaymentsClient(getContext(),
+                        new Wallet.WalletOptions.Builder()
+                                .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                                .build());
+
+        payButton = view.findViewById(R.id.buy_button);
+        payButton.setEnabled(false);
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                payWithGooglePay();
+            }
+        });
+
+        isReadyToPay();
 
         return view;
     }
+
+    private void payWithGooglePay() {
+        PaymentDataRequest request = createPaymentDataRequest();
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    PaymentsClient.loadPaymentData(request),
+                    getActivity(),
+                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
+    }
+
+    private void isReadyToPay() {
+        IsReadyToPayRequest request = IsReadyToPayRequest.newBuilder()
+                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                .build();
+        Task<Boolean> task = PaymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener(
+                new OnCompleteListener<Boolean>() {
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        try {
+                            boolean result =
+                                    task.getResult(ApiException.class);
+                            if(result) {
+                                payButton.setEnabled(true);
+                            } else {
+                                payButton.setEnabled(false);
+                            }
+                        } catch (ApiException exception) {
+                            Toast.makeText(getActivity(),
+                                    "Exception: " + exception.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        String rawToken = paymentData.getPaymentMethodToken().getToken();
+
+                        Token stripeToken = Token.fromString(rawToken);
+
+                        if (stripeToken != null) {
+
+                            Toast.makeText(getContext(),
+                                    "Got token " + stripeToken.toString(), Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getContext(),
+                                "Canceled", Toast.LENGTH_LONG).show();
+
+                        break;
+                    case AutoResolveHelper.RESULT_ERROR:
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        Toast.makeText(getContext(),
+                                "Got error " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+
+                        break;
+                    default:
+                }
+                break;
+            default:
+        }
+    }
+
+    private PaymentMethodTokenizationParameters createTokenizationParameters() {
+        return PaymentMethodTokenizationParameters.newBuilder()
+                .setPaymentMethodTokenizationType(WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+                .addParameter("gateway", "stripe")
+                .addParameter("stripe:publishableKey", "pk_test_RRggAlQdtWBJBMYlhvjAQnFZ")
+                .addParameter("stripe:version", "5.1.1")
+                .build();
+    }
+
+    private PaymentDataRequest createPaymentDataRequest() {
+        amount = numberPicker.getValue();
+        PaymentDataRequest.Builder request =
+                PaymentDataRequest.newBuilder()
+                        .setTransactionInfo(
+                                TransactionInfo.newBuilder()
+                                        .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                                        .setTotalPrice(String.valueOf(amount) + ".00")
+                                        .setCurrencyCode("GBP")
+                                        .build())
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                        .setCardRequirements(
+                                CardRequirements.newBuilder()
+                                        .addAllowedCardNetworks(Arrays.asList(
+                                                WalletConstants.CARD_NETWORK_VISA,
+                                                WalletConstants.CARD_NETWORK_MASTERCARD))
+                                        .build());
+
+
+
+        Toast.makeText(getActivity(),
+                "Thank-you for donating " + String.valueOf(amount)+".00", Toast.LENGTH_LONG).show();
+
+        request.setPaymentMethodTokenizationParameters(createTokenizationParameters());
+        return request.build();
+    }
+
 
 }
